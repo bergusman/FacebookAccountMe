763edcb4ad35bf68ef06ca3eaa0d997fddbaf43c
Vitaliy Berg <bergusman@gmail.com>
2013-07-22 16:37:20 +0400

    Initial Commit

---
+++ FacebookAccountMe/VBViewController.h
@@ -0,0 +1,13 @@
+//
+//  VBViewController.h
+//  FacebookAccountMe
+//
+//  Created by Vitaly Berg on 22.07.13.
+//  Copyright (c) 2013 Vitaly Berg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VBViewController : UIViewController
+
+@end
